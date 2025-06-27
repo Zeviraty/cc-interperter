8ab08bd3382957bd37d1e896f751c7f135adac1c
zeviraty <zeviraty@gmail.com>
2025-06-27 13:40:29 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -71,7 +71,7 @@ def scantoken(chars):
         next_c = chars.peek()
         if next_c == '=':
             c += chars.next()
-    if c == " " or c == "   ":
+    if c == " " or c == "\t":
         return ""
     if c == '/':
         next_c = chars.peek()
